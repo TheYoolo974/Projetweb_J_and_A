728044618391ea4bed4e72833b68fc5990ecef5f
Joshua Ofori <oforijoshua37@gmail.com>
2020-11-08 16:11:38 +0100

    answers

--- sql/questionbank.sql
+++ sql/questionbank.sql
@@ -39,7 +39,11 @@ CREATE TABLE `question` (
   `question_title` varchar (255) NOT NULL COMMENT 'title of the question',
   `question_quizz_id` int (11) NOT NULL COMMENT 'link question quizz',
   `question_input_type` varchar (255) NOT NULL COMMENT 'input of the question',
+<<<<<<< Updated upstream
   `related_image` boolean (1) NOT NULL COMMENT 'image or not'
+=======
+  `related_image` tinyint (1) NULL COMMENT 'image or not'
+>>>>>>> Stashed changes
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- ------------------------------------------------- --------
