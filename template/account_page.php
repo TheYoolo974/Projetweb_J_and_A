f55a1352a3f598dfbf35672ccafc9b97a7981603
Joshua Ofori <oforijoshua37@gmail.com>
2020-11-20 19:12:09 +0100

    fixed account page error

--- template/account_page.php
+++ template/account_page.php
@@ -14,7 +14,7 @@
 <p><font color="white">Please log in</font></p>
 </section>
     <section class="logincontent1">  
-    <form  action="index.php?page=account_page.php" method="post">
+    <form  action="index.php?page=account_page" method="post">
 
         <div class="container">
           <label for="lastname"><b>First Name</b></label>
